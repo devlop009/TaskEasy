785456f941f97689c485814512a11208c9c05a90
Dev Patel <000485702@student.vcc.ca>
2024-11-04 19:16:26 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      runtime: 'nodejs18.20.4
+    })
   },
   preprocess: vitePreprocess()
 };
+
 export default config;
